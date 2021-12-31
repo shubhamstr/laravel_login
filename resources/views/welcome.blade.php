5672b0ee6aa70d27bb64731aff27c8082426e66d
shubhamstr <shubham.sutar@vinnovatetechnologies.com>
2021-12-31 22:20:52 +0530

    homepage msg update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,16 +25,19 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                        <a href="{{ url('/home') }}" style="color:#fff;" class="text-sm text-gray-700 underline">Home</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+                        <a href="{{ route('login') }}" style="color:#fff;" class="text-sm text-gray-700 underline">Login</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                            <a href="{{ route('register') }}" style="color:#fff;" class="ml-4 text-sm text-gray-700 underline">Register</a>
                         @endif
                     @endauth
                 </div>
             @endif
+
+            
+            <h1 style="color:#fff;">Laravel Login</h1>
 
             
         </div>
